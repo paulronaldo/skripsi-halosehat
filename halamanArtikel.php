cb6f281b91a3756bc0d11b900c8ce5bfbf43cc41
Paul R Y Pasaribu <45089304+paulronaldo@users.noreply.github.com>
2022-11-25 15:11:52 +0700

    Update halamanArtikel.php

--- halamanArtikel.php
+++ halamanArtikel.php
@@ -16,7 +16,7 @@
       integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
       crossorigin="anonymous"
     /> -->
-    <title>Obat</title>
+    <title>Artikel</title>
   </head>
   <body>
     <!-- header -->
